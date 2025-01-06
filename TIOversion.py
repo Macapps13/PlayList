263929bea8408ec5d89f2b66c591d2bd08fc0914
Macapps13 <ben.alvaro@icloud.com>
2025-01-06 15:26:19 +1100

    Add Game class with attributes and string representation

--- TIOversion.py
+++ TIOversion.py
@@ -4,3 +4,17 @@
 # This should be used to test the logic of the program, and to test the output of the program.
 
 
+# establishing the Game class
+
+class Game:
+    def __init__(self, name, genre, rating):
+        self.name = name
+        self.genre = genre
+        self.rating = rating
+
+    def __str__(self):
+        return f"Name: {self.name}, Genre: {self.genre}, Rating: {self.rating}"
+    
+game1 = Game("The Witcher 3", "RPG", 10)
+
+print(game1)
