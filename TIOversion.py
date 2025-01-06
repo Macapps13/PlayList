3d7d4dbb526f1460b0b8ec11a36764bc0cbf1a90
Macapps13 <ben.alvaro@icloud.com>
2025-01-06 15:23:54 +1100

    Add HTML structure, JavaScript for date display, and CSS styling

---
+++ TIOversion.py
@@ -0,0 +1,6 @@
+# This is a Terminal version of the program, for testing and logic purposes.
+# It is not intended to be used by the user.
+# It is a simple version of the program that allows the user to input the data manually.
+# This should be used to test the logic of the program, and to test the output of the program.
+
+
